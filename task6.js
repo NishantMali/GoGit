7caf701616de7cbb7b363c74adfe57372fe88253
NishantMali <nish.mali2511@gmail.com>
2023-01-25 03:50:14 +0530

    task6

---
+++ task6.js
@@ -0,0 +1,26 @@
+// Queryselector
+
+
+// var itm = document.querySelector('.list-group-item');
+// // itm.style.backgroundColor = 'green';
+
+// var seconditem = document.querySelector('.list-group-item:nth-child(2)');
+// seconditem.style.backgroundColor = 'green';
+
+// var thirditem = document.querySelector('.list-group-item:nth-child(3)');
+// thirditem.style.display = 'None';
+
+// queryselectorall
+
+
+
+// var odd = document.querySelectorAll('li:nth-child(odd)');
+
+// for(var i= 0;i<odd.length ; i++){
+//     odd[i].style.backgroundColor = 'green';
+// }
+
+
+// var seconditm = document.querySelectorAll('li');
+// // console.log(seconditm);
+// seconditm[1].style.color = 'green';
